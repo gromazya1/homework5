0119ffcfdd674b523a553ffa7be1e1a53be3aec2
Kirill Gromazin <167010706+gromazya1@users.noreply.github.com>
2024-04-17 19:47:21 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ["яблоко", "апельсин", "банан", "виноград", "груша"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:5])
+my_list[3] = "персик"
+print(my_list)
+
+my_dict = {"VK": 12345678, "Telegram": 87654321, "WhatsApp": 135790}
+print(my_dict)
+print(my_dict["VK"])
+my_dict["VK"] = 2281337
+print(my_dict)
